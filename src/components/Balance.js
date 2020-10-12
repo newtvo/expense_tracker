07cfe79d52f297b2d3cff2da295ccb39a3131aef
Newt vo <nhutmvo1997@gmail.com>
2020-10-12 16:43:07 -0400

    Create Box Shadow for the Containers

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -11,7 +11,7 @@ export const Balance = () => {
 
     return (
         <>
-             <h4>Your Balance</h4>
+             <h2>Your Balance</h2>
             <h1>${total}</h1>
         </>
     )
